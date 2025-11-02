32389d97dbbd73bd351aa7e6b460f7c5eb7a4914
calistakiraa <calista.kira@students.amikom.ac.id>
2025-11-02 20:54:04 +0700

    praktikum 05

--- app/src/main/java/com/example/kiralune/Dashboard.kt
+++ app/src/main/java/com/example/kiralune/Dashboard.kt
@@ -1,6 +1,5 @@
 package com.example.kiralune
 
-
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -11,29 +10,36 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kiralune.R
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.GridLayoutManager
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard)
 
+        val dataList = ArrayList<Mahasiswa>()
+        dataList.add(Mahasiswa("Calista Sasikirana", "23SI12345"))
+        dataList.add(Mahasiswa("Andi Pratama", "23SI12346"))
+        dataList.add(Mahasiswa("Budi Santoso", "23SI12347"))
+
         val btn_web: Button = findViewById(R.id.btn_web)
 
         val username = intent.getStringExtra("nama" )
+
+        val rvMahasiswa: RecyclerView = findViewById(R.id.rv_mahasiswa)
+
+        rvMahasiswa.layoutManager = GridLayoutManager(this, 2)
+
+        val adapter = MahasiswaAdapter(dataList)
+
+        rvMahasiswa.adapter = adapter
 
         btn_web.setOnClickListener {
             val url = "https://amikom.ac.id"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
-        }
-
-        if (savedInstanceState == null) {
-            val fragment = DashboardFragment()
-
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, fragment)
-                .commit()
         }
     }
 }
---
+++ app/src/main/java/com/example/kiralune/Mahasiswa.kt
@@ -0,0 +1,6 @@
+package com.example.kiralune
+
+data class Mahasiswa(
+    val nama: String,
+    val nim: String
+)
